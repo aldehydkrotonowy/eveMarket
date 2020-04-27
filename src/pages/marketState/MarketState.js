51ad56ee57bc553a11f07023adbf68a9d6bb8815
Norbert <norbert.gajda@gmail.com>
2020-04-27 21:33:54 +0200

    clean and remove not necesary files

--- src/pages/marketState/MarketState.js
+++ src/pages/marketState/MarketState.js
@@ -1,22 +1,12 @@
 import React from "react";
-import regionsList from "../../assets/base/regionsList.json";
-import rawOreList from "../../assets/base/rawOreList.json";
-import aaaa from "../../assets/base/systemsAndsItsDetails.json";
-import bbbb from "../../assets/base/systemsSecAbove0.4.json";
 
 class MarketState extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      regionList: regionsList,
-      rawOreList: rawOreList,
-      tst: aaaa,
-    };
+    this.state = {};
   }
 
   render() {
-    const i = this.state.tst.find((a) => a.name === "Nifflung");
-    console.log(this.state.tst.length, i, bbbb.length);
     return null;
   }
 }
